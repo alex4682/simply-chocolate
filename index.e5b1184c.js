cdba28a475d0019b743154d08815b00684a6c613
alex4682 <134195119+alex4682@users.noreply.github.com>
2024-03-20 16:10:01 +0000

    Deploying to gh-pages from @ alex4682/simply-chocolate@e0b3b6528d284c2ebaf612232da4fc530b60e137 🚀

---
+++ index.e5b1184c.js
@@ -0,0 +1,2 @@
+new Splide("#mySlider",{perPage:4,gap:"18px",type:"loop",focus:0,omitEnd:!0,pagination:!0,breakpoints:{768:{pagination:!0,perPage:1,arrows:!1},1200:{perPage:2,arrows:!0}}}).mount();
+//# sourceMappingURL=index.e5b1184c.js.map
